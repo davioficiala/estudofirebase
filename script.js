09864f990930676608d0dcd1d99d85ed274f22e6
davioficiala <daviwalker813@gmail.com>
2026-06-05 20:13:36 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 // Conecta o site ao projeto Firebase
-// usando as configurações do firebaseConfig
 const app = initializeApp(firebaseConfig);
+
+// Abre acesso ao banco de dados Firestore
 getFirestore()
+
+// Salva ou substitui um documento
 setDoc()
+
+// Busca um documento do banco
 getDoc()
+
+// Atualiza campos de um documento existente
 updateDoc()
+
+// Cria um documento com ID gerado automaticamente
 addDoc()
