9b4d878f16a2e36f147a8f25bcd0f3acc14207cd
davioficiala <daviwalker813@gmail.com>
2026-06-06 11:36:53 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 
 import {
@@ -8,7 +7,7 @@ import {
 } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "SUA_API_KEY",
+  apiKey: "AIzaSyAdirRN9nHaekNUjM3upyPTSBRVollrlMI",
   authDomain: "deus-fiel-7a2cc.firebaseapp.com",
   projectId: "deus-fiel-7a2cc",
   storageBucket: "deus-fiel-7a2cc.firebasestorage.app",
@@ -16,25 +15,19 @@ const firebaseConfig = {
   appId: "1:746071893459:web:c7a31b6e8ad49709187b26"
 };
 
-// Conecta ao Firebase
 const app = initializeApp(firebaseConfig);
-
-// Conecta ao Firestore
 const db = getFirestore(app);
 
-// Busca o documento
 const documento = await getDoc(
   doc(db, "teste", "usuarios")
 );
 
-// Mostra o valor na tela
 if (documento.exists()) {
 
   document.getElementById("saldoDisponivel").innerText =
-    documento.data().saldoDisponivel.toLocaleString("pt-BR", {
-      style: "currency",
-      currency: "BRL"
-    });
+    "R$ " + documento.data().saldoDisponivel;
+
+  console.log(documento.data());
 
 } else {
 
