439551f94c9f7b4da0ea8ee4b2e63c4349fe9f77
davioficiala <daviwalker813@gmail.com>
2026-06-06 11:02:03 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,28 @@
-// Conecta o site ao projeto Firebase
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+
+import {
+  getFirestore,
+  doc,
+  setDoc,
+  getDoc
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "SUA_API_KEY",
+  authDomain: "deus-fiel-7a2cc.firebaseapp.com",
+  projectId: "deus-fiel-7a2cc",
+  storageBucket: "deus-fiel-7a2cc.firebasestorage.app",
+  messagingSenderId: "746071893459",
+  appId: "1:746071893459:web:c7a31b6e8ad49709187b26"
+};
+
+// Conecta ao Firebase
 const app = initializeApp(firebaseConfig);
 
-// Abre acesso ao banco de dados Firestore
+// Conecta ao Firestore
 const db = getFirestore(app);
 
-// Salva ou substitui um documento
+// Salva teste
 await setDoc(
   doc(db, "usuarios", "usuario1"),
   {
@@ -13,40 +31,13 @@ await setDoc(
   }
 );
 
-console.log("Documento salvo!");
-
-// Busca um documento do banco
+// Lê o documento
 const documento = await getDoc(
   doc(db, "usuarios", "usuario1")
 );
 
-// Mostra o saldo na tela
+// Mostra na tela
 if (documento.exists()) {
-
   document.getElementById("saldoDisponivel").innerText =
     "R$ " + documento.data().saldo;
-
-  console.log(documento.data());
-
 }
-
-// Atualiza campos de um documento existente
-await updateDoc(
-  doc(db, "usuarios", "usuario1"),
-  {
-    saldo: 2500
-  }
-);
-
-console.log("Documento atualizado!");
-
-// Cria um documento com ID gerado automaticamente
-await addDoc(
-  collection(db, "usuarios"),
-  {
-    nome: "Maria",
-    saldo: 1500
-  }
-);
-
-console.log("Documento criado com ID automático!");
