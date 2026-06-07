abafec6aeecd03601b07e2f5503c9631c305ce18
davioficiala <daviwalker813@gmail.com>
2026-06-07 10:33:17 -0300

    Update script.js

--- script.js
+++ script.js
@@ -5,26 +5,10 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/fireba
 import {
   getFirestore,
   doc,
-  getDoc,
-  setDoc,
-  updateDoc,
-  addDoc,
-  collection,
-  deleteDoc
+  getDoc
 } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
-// PASSO 3 - Importar Authentication
-import {
-  getAuth,
-  createUserWithEmailAndPassword,
-  signInWithEmailAndPassword,
-  signOut,
-  onAuthStateChanged,
-  updateProfile,
-  deleteUser
-} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
-
-// PASSO 4 - Configuração Firebase
+// PASSO 3 - Configuração Firebase
 const firebaseConfig = {
   apiKey: "AIzaSyAdirRN9nHaekNUjM3upyPTSBRVollrlMI",
   authDomain: "deus-fiel-7a2cc.firebaseapp.com",
@@ -34,63 +18,39 @@ const firebaseConfig = {
   appId: "1:746071893459:web:c7a31b6e8ad49709187b26"
 };
 
-// PASSO 5 - Iniciar Firebase
+// PASSO 4 - Iniciar Firebase
 const app = initializeApp(firebaseConfig);
 
-// PASSO 6 - Abrir Firestore
+// PASSO 5 - Abrir Firestore
 const db = getFirestore(app);
 
-// PASSO 7 - Abrir Authentication
-const auth = getAuth(app);
+// PASSO 6 - Referência do Documento
+const docRef = doc(db, "teste", "usuarios");
 
-// PASSO 8 - Referência do Documento
-const docRef = doc(db, "usuarios", "cliente1");
-
-// PASSO 9 - Ler Documento
+// PASSO 7 - Ler Documento
 const documento = await getDoc(docRef);
 
-document.getElementById("saldoDisponivel").innerText =
-"R$ " + documento.data().saldoDisponivel;
+if (documento.exists()) {
 
-document.getElementById("gastosHoje").innerText =
-"R$ " + documento.data().gastosHoje;
+  document.getElementById("saldoDisponivel").innerText =
+    "R$ " + documento.data().saldoDisponivel;
 
-document.getElementById("limiteDiario").innerText =
-"R$ " + documento.data().limiteDiario;
+  document.getElementById("gastosHoje").innerText =
+    "R$ " + documento.data().gastosHoje;
 
-document.getElementById("restanteHoje").innerText =
-"R$ " + documento.data().restanteHoje;
+  document.getElementById("limiteDiario").innerText =
+    "R$ " + documento.data().limiteDiario;
 
-// PASSO 10 - Salvar Documento
-setDoc(docRef, dados);
+  document.getElementById("restanteHoje").innerText =
+    "R$ " + documento.data().restanteHoje;
 
-// PASSO 11 - Atualizar Documento
-updateDoc(docRef, dados);
+} else {
 
-// PASSO 12 - Adicionar Documento Automático
-addDoc(collectionRef, dados);
+  document.getElementById("saldoDisponivel").innerText = "R$ 0";
+  document.getElementById("gastosHoje").innerText = "R$ 0";
+  document.getElementById("limiteDiario").innerText = "R$ 0";
+  document.getElementById("restanteHoje").innerText = "R$ 0";
 
-// PASSO 13 - Excluir Documento
-deleteDoc(docRef);
+  console.log("Documento não encontrado");
 
-// PASSO 14 - Criar Usuário
-createUserWithEmailAndPassword(auth, email, senha);
-
-// PASSO 15 - Fazer Login
-signInWithEmailAndPassword(auth, email, senha);
-
-// PASSO 16 - Ver Usuário Logado
-onAuthStateChanged(auth, (user) => {
-
-});
-
-// PASSO 17 - Atualizar Perfil
-updateProfile(user, {
-  displayName: "Nome"
-});
-
-// PASSO 18 - Sair da Conta
-signOut(auth);
-
-// PASSO 19 - Excluir Usuário
-deleteUser(user);
+}
